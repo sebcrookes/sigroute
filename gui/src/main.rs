1f46e8cf49177c8b13ad8180f374292d70335cc9
Sebastian Crookes <309699783+sebcrookes@users.noreply.github.com>
2026-06-12 18:10:37 +0100

    Added test IPC between the daemon and the client using zbus (Rust D-Bus implementation) - quite messy code for now

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,20 +1,59 @@
 use gtk4::*;
 use gtk4::prelude::*;
 
+use zbus::Connection;
+use zbus::proxy;
+
+use tokio;
+
+#[proxy(
+    interface = "uk.co.sebcrookes.Sigroute",
+    default_service = "uk.co.sebcrookes.Sigroute",
+    default_path = "/uk/co/sebcrookes/Sigroute",
+    gen_blocking = true
+)]
+trait AutomationAPI {
+    fn list_all(&self) -> zbus::Result<String>;
+}
+
+async fn list_automations() -> zbus::Result<String> {
+    let connection = Connection::session().await?;
+    let proxy = AutomationAPIProxy::new(&connection).await?;
+
+    let reply = proxy.list_all().await;
+
+    return reply;
+}
+
 fn main() {
+
+    /* Requesting the list of automations from the daemon (sigrouted) */
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    let result = rt.block_on(list_automations());
+
+    match result {
+        Ok(retv) => println!("{retv}"),
+        Err(_e) => println!("Error!"),
+    };
+
+    /* Initialising the GUI */
+
     let app = Application::builder()
-        .application_id("uk.co.sebcrookes.sigroute")
+        .application_id("uk.co.sebcrookes.SigrouteGUI")
         .build();
     
     app.connect_activate(build_ui);
-                
+
     app.run();
 }
 
 fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Sigroute GUI")
+        .title("Sigroute")
+        .default_width(420)
+        .default_height(320)
         .build();
     
     window.present();
