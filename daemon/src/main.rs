c64eb628322056509474bdb953fb8647306509d1
Sebastian Crookes <71226595+sebcrookes@users.noreply.github.com>
2026-06-12 18:10:37 +0100

    Added test IPC between the daemon and the client using zbus (Rust D-Bus implementation) - quite messy code for now

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,3 +1,28 @@
+use zbus::blocking::connection;
+use zbus::interface;
+
+struct AutomationAPI;
+
+#[interface(name = "uk.co.sebcrookes.Sigroute")]
+impl AutomationAPI {
+    fn list_all(&self) -> String {
+        return "This is a test string being returned by sigrouted".to_string();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("[Info] - sigrouted running...");
+    let _ = run_api();
+}
+
+fn run_api() -> zbus::Result<()> {
+    let automation_api = AutomationAPI;
+    let _connection = connection::Builder::session()?
+        .name("uk.co.sebcrookes.Sigroute")?
+        .serve_at("/uk/co/sebcrookes/Sigroute", automation_api)?
+        .build()?;
+
+    std::thread::park();
+
+    Ok(())
 }
